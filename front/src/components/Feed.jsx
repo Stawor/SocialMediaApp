6158ec916ea84aebda84938f7cb30b903a569b30
Stawor <staworkogabriel@gmail.com>
2023-11-22 14:17:49 +0100

    url change

--- front/src/components/Feed.jsx
+++ front/src/components/Feed.jsx
@@ -20,7 +20,7 @@ export default function Feed() {
 	async function getFeed() {
 		if (user) {
 			const response = await axios.get(
-				`http://localhost:3000/api/posts/timeline/all/${user._id}`,
+				`https://socialmediaapp-ydzs.onrender.com/posts/timeline/all/${user._id}`,
 				{ headers: { Authorization: `Bearer ${TokenCookie}` } }
 			);
 			setPosts(
