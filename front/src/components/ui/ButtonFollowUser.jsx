a11145da6392e5188d941bc31602d9a1092edc42
Stawor <staworkogabriel@gmail.com>
2023-11-22 16:03:00 +0100

    fecth url changed

--- front/src/components/ui/ButtonFollowUser.jsx
+++ front/src/components/ui/ButtonFollowUser.jsx
@@ -5,14 +5,14 @@ import { UserContext } from "../../contexts/user-context";
 export default function ButtonFollowUser({ userId }) {
 	const { user } = useContext(UserContext);
 
-	const handleClick = () => {
-		axios.put(
+	const handleClick = async () => {
+		await axios.put(
 			`https://socialmediaapp-ydzs.onrender.com/api/users/${userId}/follow`,
 			{
 				id: user._id,
 			}
 		);
-		// window.location.reload();
+		window.location.reload();
 	};
 
 	return <button onClick={handleClick}>Follow</button>;
--- front/src/components/ui/ButtonUnfollow.jsx
+++ front/src/components/ui/ButtonUnfollow.jsx
@@ -5,14 +5,14 @@ import { UserContext } from "../../contexts/user-context";
 export default function ButtonUnfollow({ userId }) {
 	const { user } = useContext(UserContext);
 
-	const handleClick = () => {
-		axios.put(
+	const handleClick = async () => {
+		await axios.put(
 			`https://socialmediaapp-ydzs.onrender.com/api/users/${userId}/unfollow`,
 			{
 				id: user._id,
 			}
 		);
-		// window.location.reload();
+		window.location.reload();
 	};
 
 	return <button onClick={handleClick}>Unfollow</button>;
